570b2408e1b8e71deb3c65623150c63fbdbccd2f
Alex Merecka <merecka@gmail.com>
2019-04-16 00:24:33 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if (self.side1 + self.side2) < self.side3 || (self.side1 + self.side3) < self.side2 || (self.side2 + self.side3) < self.side1
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+        puts error.message
+      end
+    elsif self.side1 != self.side2 && self.side1 != self.side3 && self.side2 != self.side3
+      :scalene
+    elsif self.side1 == self.side2 || self.side1 == self.side3 || self.side2 == self.side3
+      :isosceles
+    elsif self.side1 == self.side2 && self.side1 == self.side3 && self.side2 == self.side3
+      :equilateral
+    end
+
+  end
+
+  class TriangleError
+    def message
+      "Invalid Triangle: The length of any two sides must be larger than the third side"
+    end
+
 end
